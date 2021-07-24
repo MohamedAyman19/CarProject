04a316792e9852691918fea3275ebbfb3d03d8f6
MohamedAyman19 <87302157+MohamedAyman19@users.noreply.github.com>
2021-07-25 01:34:06 +0200

    Add files via upload

---
+++ Global_Int_Driver/GI_INT.h
@@ -0,0 +1,19 @@
+/*
+ * GI_INT.h
+ *
+ * Created: 23-Jul-21 11:12:10 PM
+ *  Author: ayman
+ */ 
+
+
+#ifndef GI_INT_H_
+#define GI_INT_H_
+
+void GI_vdEnableGI (void);
+void GI_vdDisableGI (void);
+
+#define I_BIT 7
+
+
+
+#endif /* GI_INT_H_ */
---
+++ Global_Int_Driver/GI_Registers.h
@@ -0,0 +1,16 @@
+/*
+ * GI_REG.h
+ *
+ * Created: 23-Jul-21 11:12:34 PM
+ *  Author: ayman
+ */ 
+
+
+#ifndef GI_REGISTERS_H_
+#define GI_REGISTERS_H_
+
+#define SREG_REGISTER  *( ( volatile uint8_t* ) 0x5F )
+
+
+
+#endif /* GI_REG_H_ */
